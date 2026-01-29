419249eb54082a302c49df19c36b9da026b153c2
zisanahmed97 <zisanahmed97@gmail.com>
2026-01-29 10:58:54 +0600

    added object file

---
+++ object.js
@@ -0,0 +1,16 @@
+const bottle={
+    color: 'white',
+    drink:'water',
+    price : 45 ;
+    
+}
+
+const subject={
+    name:'Biology',
+    teacher:'rasheda mam',
+    chapter:['first','second','third'],
+    exam:{
+        
+    }
+
+}
